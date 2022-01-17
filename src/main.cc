95e59df5b221d0095496d7b0f22f7234c51a0d18
Antoine Julien <antoine.julien@epita.fr>
2022-01-17 21:06:14 +0100

    Main command line args, save result in file

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,17 @@
 
 int main(int argc, char const *argv[])
 {
+    if (argc != 3)
+    { 
+        std::cout << "Usage: [input file] [out file]\n";
+        return -1;
+    }
+
+    std::string in_file = argv[1];
+    std::string out_file = argv[2];
+
     auto board = Board();
-    board.parse("../3x3.txt");
+    board.parse(in_file);
     std::cout << board;
 
     Solver s{board};
@@ -18,6 +27,8 @@ int main(int argc, char const *argv[])
         std::cout << "Solution not found in time :\n";
 
     std::cout << s.get_board();
+
+    s.get_board().save(out_file);
 
     return 0;
 }
